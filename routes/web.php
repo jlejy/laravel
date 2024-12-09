bdc1757ad8c9c159b7a114f85768a1af64e00a44
jiyoon <lvyjyn130019@gmail.com>
2024-12-09 09:56:08 +0900

    no message

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware('auth')->group(function () {
 Route::get('/aa', [abcController::class, 'index']);
 
 Route::get('hello', function() {
-    return response('Hello World'); //helper = 함수입니다.
+    return response('Hello World'); //helper = 함수입니다.dd
 });
 
 require __DIR__.'/auth.php';
